6c9bc7dd8a0214369aa47076d523d6cc4e72602f
Fe4ester <xdxdxd4305@gmail.com>
2024-07-17 21:57:59 +0300

    start...

--- main.py
+++ main.py
@@ -2,17 +2,8 @@ import requests
 import bs4
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.ui import WebDriverWait
-
-
-def wait_element(browser, delay_seconds=1, by=By.CLASS_NAME, value=None):
-    return WebDriverWait(browser, delay_seconds).until(
-        expected_conditions.presence_of_element_located((by, value))
-    )
-
+from pprint import pprint
 
 path = Service(ChromeDriverManager().install())
 
@@ -25,33 +16,46 @@ browser.get('https://spb.hh.ru/search/vacancy?text=python&area=1&area=2')
 
 html = browser.page_source
 
-salary = []
-sorted_salary = []
+total_vacancy = []
 
 soup = bs4.BeautifulSoup(html, features='lxml')
 main_tag = soup.find('main', class_='vacancy-serp-content')
 main_tags = main_tag.find_all('div')
 
 for div in main_tags:
-    next_tag = div.find('span',
-                        class_='fake-magritte-primary-text--Hdw8FvkOzzOcoR4xXWni compensation-text--kTJ0_rp54B2vNeZ3CTt2 separate-line-on-xs--mtby5gO4J0ixtqzW38wh')
+    salary_tag = div.find('span',
+                          class_='fake-magritte-primary-text--Hdw8FvkOzzOcoR4xXWni compensation-text--kTJ0_rp54B2vNeZ3CTt2 separate-line-on-xs--mtby5gO4J0ixtqzW38wh')
 
-    if next_tag:
-        hard_text = next_tag.text
-        compensation_text = hard_text.replace('\u202f', ' ').replace('\xa0', ' ')
+    description_tag = div.find('span', class_='vacancy-name--c1Lay3KouCl7XasYakLk serp-item__title-link')
+    link_tag = div.find('a', class_='bloko-link')
+
+    if salary_tag:
+        hard_text = salary_tag.text
+        salary_text = hard_text.replace('\u202f', ' ').replace('\xa0', ' ')
     else:
-        compensation_text = None
+        salary_text = None
 
-    salary.append(compensation_text)
+    if description_tag:
+        description_text = description_tag.text
+    else:
+        description_text = None
+
+    if link_tag:
+        link_text = link_tag.get('href')
+    else:
+        link_text = None
+
+    if description_text is not None:
+        if 'django' in description_text.lower() or 'flask' in description_text.lower():
+
+            total_vacancy.append({
+                'description': description_text,
+                'salary': salary_text,
+                'link': link_text
+            })
+
+
 
 browser.quit()
 
-for i in salary:
-    if i is not None:
-        if '$' in i:
-            sorted_salary.append(i)
-            print(i)
-
-print(sorted_salary)
-print(len(salary))
-print(len(sorted_salary))
+pprint(total_vacancy)
